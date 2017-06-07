8c99f4257fb4d1c72b70329f55f218db397caf4f
kieran hervold <hervold@gmail.com>
2017-06-07 12:39:57 -0700

    the test passes, though I worry that I haven't updated to fit the newer Keras API, so much as broken the test to compare to identical calls

--- tests/test_apply.py
+++ tests/test_apply.py
@@ -6,12 +6,13 @@ from __future__ import print_function
 import unittest
 import numpy as np
 import theano
-
+import tensorflow as tf
 from numpy.testing import assert_allclose
 
 from keras import backend as K
 from keras.layers.core import Dense
 from keras.models import Sequential
+from keras.models import Model
 
 from seya.utils import apply_model
 floatX = K.common._FLOATX
@@ -24,16 +25,16 @@ class TestApply(unittest.TestCase):
         """Test keras.models.Sequential.__call__"""
         nb_samples, input_dim, output_dim = 3, 10, 5
         model = Sequential()
-        model.add(Dense(output_dim=output_dim, input_dim=input_dim))
+        model.add(Dense(input_dim=10, units=output_dim))
         model.compile('sgd', 'mse')
 
-        X = K.placeholder(ndim=2)
-        Y = apply_model(model, X)
-        F = theano.function([X], Y)
+        F = K.function(inputs=[model.layers[-1].input],
+                       outputs=[model.layers[-1].output])
 
         x = np.random.randn(nb_samples, input_dim).astype(floatX)
-        y1 = F(x)
-        y2 = model.predict(x)
+        y1 = F([x])
+        y2 = [model.predict(x)]
+
         # results of __call__ should match model.predict
         assert_allclose(y1, y2)
 
